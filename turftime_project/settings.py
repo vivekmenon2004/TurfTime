a33b8082ac816557b735cb0db552e5ede204e045
vivek <vivekmenonv2004@gmail.com>
2026-03-25 20:22:17 +0530

    Fix ALLOWED_HOSTS for ALB health checks

--- turftime_project/settings.py
+++ turftime_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-gt6&$d67ldbwryv9ltp)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
